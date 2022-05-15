62f348a571a9639b659c9e84601072b46e72d300
leew0nseok <zzang50aa@gmail.com>
2022-05-15 16:18:48 +0900

    Study Java First

---
+++ src/GuGu.java
@@ -0,0 +1,14 @@
+public class GuGu {
+    public void dan(int n){
+        for(int i =1; i <10; i++){
+            System.out.println(n * i);
+        }
+    }
+    public static void main(String[] args){
+        GuGu gugu = new GuGu();
+        for(int j =2; j <10; j++){
+            gugu.dan(j);
+        }
+
+    }
+}
