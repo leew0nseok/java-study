cff4df33169a391497af10025ab5309f51487f72
leew0nseok <zzang50aa@gmail.com>
2022-06-04 20:34:20 +0900

    StringBuffer

--- src/Sample.java
+++ src/Sample.java
@@ -6,5 +6,12 @@ public class Sample {
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
+
+        StringBuffer sb = new StringBuffer();  // StringBuffer 객체 sb 생성
+        sb.append("hello");
+        sb.append(" ");
+        sb.append("jump to java");
+        String result = sb.toString();
+        System.out.println(result);
     }
 }
