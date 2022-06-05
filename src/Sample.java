156b517495987c2f08ad3c16de0113a2f96ce4f8
leew0nseok <zzang50aa@gmail.com>
2022-06-05 14:30:42 +0900

    Array

--- src/Sample.java
+++ src/Sample.java
@@ -13,5 +13,10 @@ public class Sample {
         sb.append("jump to java");
         String result = sb.toString();
         System.out.println(result);
+
+        String[] weeks = {"월", "화", "수", "목", "금", "토", "일"};
+        for (int i=0; i<weeks.length; i++) {
+            System.out.println(weeks[i]);
+        }
     }
 }
