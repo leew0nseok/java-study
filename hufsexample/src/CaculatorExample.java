5883dac4e626f259502e4495ca988075c993d29d
leew0nseok <zzang50aa@gmail.com>
2022-10-14 11:13:27 +0900

    hufs

---
+++ hufsexample/src/CaculatorExample.java
@@ -0,0 +1,12 @@
+public class CaculatorExample {
+    public static void main(String[] args){
+
+        Calculators cal = new Calculators();
+
+        System.out.println(Calculators.abs(-5));
+        System.out.println(Calculators.max(10, 8));
+        System.out.println(Calculators.min(-3, -8));
+    }
+}
+
+
---
+++ hufsexample/src/Calculators.java
@@ -0,0 +1,16 @@
+public class Calculators {
+    static int max(int a, int b){
+        if(a > b) return a;
+        else return b;
+    }
+
+    static int min(int a, int b){
+        if(a < b) return a;
+        else return b;
+    }
+
+    static int abs(int i) {
+        if (i < 0) return -i;
+        else return i;
+    }
+}
