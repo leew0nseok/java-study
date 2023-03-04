40ab82d448c65c6e47b649efa96e5481bda4fddd
leew0nseok <zzang50aa@gmail.com>
2023-03-05 00:29:11 +0900

    part 3 - chap 2

---
+++ fastcamp/design/src/com/company/design/adapter/Cleaner.java
@@ -0,0 +1,8 @@
+package com.company.design.adapter;
+
+public class Cleaner implements Electronic220V{
+    @Override
+    public void connect() {
+        System.out.println("Ã»¼Ò±â 220v on");
+    }
+}
